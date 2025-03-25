941b83350839d6abb2d69f2eba77d095ed8de4ae
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-03-25 11:41:13 -0300

    exercicios

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,5 +1,5 @@
 
-public class Exercicio2 {
+public class exercicio2 {
 
 	public static void main(String[] args) {
 
---
+++ src/exercicio3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+
+		int a, b, resultado;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite um valor inteiro: ");
+		a = sc.nextInt();
+
+		System.out.println("Digite outro valor inteiro: ");
+		b = sc.nextInt();
+
+		resultado = a + b;
+		System.out.printf("A soma dos valores corresponde a %d.", resultado);
+
+		sc.close();
+	}
+
+}
