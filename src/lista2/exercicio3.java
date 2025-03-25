030bbdbf269c5085f7b18e6d556c0ea5c20090a4
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-03-25 15:12:14 -0300

    EXPRESSÃO CONDICIONAL TERNÁRIA

---
+++ src/lista2/exercicio3.java
@@ -0,0 +1,16 @@
+package lista2;
+
+//EXPRESSÃO CONDICIONAL TERNÁRIA
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+
+		double preco = 34.5;
+		// o valor é validado na própria atribuição do valor:
+		double desconto = (preco < 20.0) ? preco + 0.1 : preco * 0.05;
+
+		System.out.print(desconto);
+
+	}
+}
