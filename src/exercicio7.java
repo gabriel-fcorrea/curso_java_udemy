399055ba1cfd3f3e2abec7bcc4029bd5695ad456
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-03-25 13:06:27 -0300

    exercicios novos udemy

---
+++ src/exercicio7.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class exercicio7 {
+
+	public static void main(String[] agrs) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int cod1, numPecas1, cod2, numPecas2;
+		double valUnit1, valUnit2, total;
+
+		System.out.print("Código: ");
+		cod1 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Número de Peças: ");
+		numPecas1 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Valor Unitário: ");
+		valUnit1 = sc.nextDouble();
+		sc.nextLine();
+
+		System.out.print("Código: ");
+		cod2 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Número de Peças: ");
+		numPecas2 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Valor Unitário: ");
+		valUnit2 = sc.nextDouble();
+		sc.nextLine();
+
+		total = (numPecas1 * valUnit1) + (numPecas2 * valUnit2);
+
+		System.out.printf("O valor total é de R$ %.2f", total);
+
+		sc.close();
+	}
+
+}
